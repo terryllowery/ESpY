1f533aa1ae539f482b01b29fde6161140f6109b3
Terry Lowery <terry@lowerytech.net>
2017-04-02 02:54:31 +0000

    Added validators from address and description presence, added min length on name to 3

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :address, :description, :name, presence: true
+  validates :name, length: {minimum: 3}
 end
