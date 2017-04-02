359a6e2625025db615a8f2a92159c650c6471826
Terry Lowery <terry@lowerytech.net>
2017-04-02 16:59:53 +0000

    Added comment model and setup associations with places and users

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
   validates :address, :description, :name, presence: true
   validates :name, length: {minimum: 3}
+
+
 end
