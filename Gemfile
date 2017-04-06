e198de2cab0388af23508699cc550708d9be00d0
Terry Lowery <terry@lowerytech.net>
2017-04-06 02:22:51 +0000

    added photo uploads and styling

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'geocoder'
 # install figaro
 gem 'figaro'
 
+# Install carrierwave for photo uploads
+gem 'carrierwave', '~> 1.0'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use postgresql as the database for Active Record
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
-    @places = Place.all.page(params[:page]).per_page(3)
+    @places = Place.all.page(params[:page]).per_page(5)
+    @photo = Photo.new
   end
 
   def new
@@ -21,6 +22,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
+    @photo = Photo.new
   end
 
   def edit
