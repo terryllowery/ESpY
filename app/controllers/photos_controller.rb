ddcbb9732a82c768399f340af98ac4b926150f92
Terry Lowery <terry@lowerytech.net>
2017-04-07 03:11:13 +0000

    added association with users in photos.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,8 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merge(place_id: @place))
+
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,15 @@
 Rails.application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      # domain: "example.com",
+      user_name: "terryllowery@gmail.com",
+      password: "Cessna177!",
+      authentication: "plain",
+      enable_starttls_auto: true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -27,7 +38,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -53,4 +64,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
+
+
 end
