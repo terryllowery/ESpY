321f5f063278c1fb52c87a7daaa492c702309d75
Terry Lowery <terry@lowerytech.net>
2017-04-08 03:26:36 +0000

    Fixed issues in production environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer:Base.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
